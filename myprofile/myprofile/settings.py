b0f59ce25351e69da615920e86e93c02535d8552
Kishan <kishanmgowdaooty25@gmail.com>
2017-02-20 19:13:59 +0530

    Django Basic Local Development settings config done

--- myprofile/myprofile/settings.py
+++ myprofile/myprofile/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myprofile.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_URL = '/static_cdn/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+MEDIA_URL = '/media_cdn/'
